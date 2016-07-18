b78216c182996bda4d01e3904654e463fced8596
Ben Barmak <ben.barmak@gmail.com>
2016-07-17 22:00:34 -0500

    Updated scripts.

--- assets/scripts/spaceshipscript.gd
+++ assets/scripts/spaceshipscript.gd
@@ -1,13 +1,19 @@
-
 extends RigidBody2D
 
+# variables
 var xspeed = 95
 var yspeed = 95
+var bodies = get_colliding_bodies()
+#####################################
 
-func _ready():
-	set_process(true)
+# custom functions
+func checkifcrash():
+	if bodies.empty():
+		pass
+	elif bodies[0].get_name() == "terrain1":
+		set_pos(Vector2(58.639641,385.803802))
 
-func _process(delta):
+func playercontrols():
 	if Input.is_action_pressed("ui_left"):
 			set_axis_velocity(Vector2(-xspeed,0))
 
@@ -19,3 +25,13 @@ func _process(delta):
 
 	if Input.is_action_pressed("ui_down"):
 		set_axis_velocity(Vector2(0,yspeed))
+####################################################
+
+func _ready():
+	set_process(true)
+
+# game loop
+func _process(delta):
+	bodies = get_colliding_bodies()
+	playercontrols()
+	checkifcrash()
